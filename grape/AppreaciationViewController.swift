2e30874b95badec628243dd8beaf6991853f53e0
Kaylie Vong <vongkaylie@gmail.com>
2021-07-01 13:38:05 -0400

    create appreciation part

---
+++ grape/AppreaciationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AppreaciationViewController.swift
+//  grape
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+
+class AppreaciationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- grape/ViewController.swift
+++ grape/ViewController.swift
@@ -9,15 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var caption: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+  
     
+    @IBAction func createC(_ sender: Any) {
+        caption.text = textBox.text
+    }
     
+    @IBAction func createP(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
     
-
-
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+        imageView.image = image
+    }
+    
+    picker.dismiss(animated: true, completion: nil)
+}
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+    picker.dismiss(animated: true, completion: nil)
+}
+}
